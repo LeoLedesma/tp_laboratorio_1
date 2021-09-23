ab55ab67142e8b68ade0e777c715af428e14bd8b
LeoLedesma <leofledesma@gmail.com>
2021-09-23 08:11:26 -0300

    tp_1

---
+++ TP_[1]/TP I/src/entradaDatos.h
@@ -0,0 +1,22 @@
+/*
+ * entradaDatos.h
+ *
+ *
+ *      Author: Leonel
+ */
+
+#ifndef ENTRADADATOS_H_
+#define ENTRADADATOS_H_
+
+
+
+#endif /* ENTRADADATOS_H_ */
+
+
+/** \brief Solicita un número al usuario, valida que sea un numero y devuelve el resultado
+ *
+ * \param mensaje[] char Es el mensaje a ser mostrado
+ * \return float El numero ingresado por el usuario
+ *
+ */
+float getFloatValidado(char mensaje[]);
